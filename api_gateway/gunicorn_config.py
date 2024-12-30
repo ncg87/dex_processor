ccf00493e62deafd3d0f0674ac99afa69ef4d8fb
ncg87 <115420183+ncg87@users.noreply.github.com>
2024-12-30 13:58:35 -0500

    added logging to api gateway

--- api_gateway/gunicorn_config.py
+++ api_gateway/gunicorn_config.py
@@ -1,3 +1,9 @@
 bind = "0.0.0.0:8000"  # Expose on all interfaces at port 8000
 workers = 1
 worker_class = "uvicorn.workers.UvicornWorker"
+
+# Logging
+accesslog = "access.log"
+errorlog = "error.log"
+loglevel = "info"
+access_log_format = '%(h)s %(l)s %(u)s %(t)s "%(r)s" %(s)s %(b)s "%(f)s" "%(a)s"'
